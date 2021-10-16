b2f9e4a6376a0961955dc31d988602d24b80fbe2
V-0ID <88525373+V-0ID@users.noreply.github.com>
2021-10-16 13:17:55 +1100

    Update GaleFighter.lua

--- FE Script Hub/StoredScripts/GaleFighter.lua
+++ FE Script Hub/StoredScripts/GaleFighter.lua
@@ -1,8 +1,19 @@
---Not Made By Me
+--Rig Checker made by me
+local CurrentRigType = game.Players.localPlayer.Character.Humanoid.RigType.Value
+
+if CurrentRigType == 1 then
+CurrentRigType = "R15"
+_G.R6 = false
+elseif CurrentRigType == 0 then
+CurrentRigType = "R6"
+ _G.R6 = true
+end
+--My code ends here
+
 --[[ Options ]]--
 _G.CharacterBug = false --Set to true if your uppertorso floats when you use the script with R15.
 _G.GodMode = true --Set to true if you want godmode.
-_G.R6 = true --Set to true if you wanna enable R15 to R6 when your R15.
+_G.R6 = nil --Set to true if you wanna enable R15 to R6 when your R15.
 --[[Reanimate]]--
 loadstring(game:HttpGet("https://gist.githubusercontent.com/M6HqVBcddw2qaN4s/fc29cbf0eda6f8b129778b441be3128f/raw/6StQ2n56PnEHMhQ9"))()
  
